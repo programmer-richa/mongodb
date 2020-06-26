e3375af6f36e1f385890849044ce64fc7d854a6e
programmer-richa <programmer.richa@gmail.com>
2020-06-26 19:18:24 +0530

    User model

---
+++ models/user.go
@@ -0,0 +1,18 @@
+package models
+
+import "gopkg.in/mgo.v2/bson"
+
+//User holds basic information regarding site user
+//MongoDB represents JSON documents in binary-encoded format called BSON behind the scenes.
+//BSON extends the JSON model to provide additional data types
+//and to be efficient for encoding and decoding within different languages
+type User struct {
+	UserName         string        `json:"username" bson:"username"`
+	FirstName        string        `json:"firstname" bson:"firstname"`
+	LastName         string        `json:"lastname" bson:"lastname"`
+	Name             string        `json:"name" bson:"name"`
+	Email            string        `json:"email" bson:"email"`
+	Password         string        `json:"password" bson:"password"`
+	SubscribeToEmail bool          `json:"subscribe" bson:"subscribe"`
+	Id               bson.ObjectId `json:"id" bson:"_id"` // Data type  bson.ObjectId is for mongodb bson format
+}
