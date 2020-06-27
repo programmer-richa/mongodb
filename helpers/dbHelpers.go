54238b34f43001c8e2d874c81aff219be28ee811
programmer-richa <programmer.richa@gmail.com>
2020-06-27 17:33:01 +0530

    Database operations method definations.

--- helpers/dbHelpers.go
+++ helpers/dbHelpers.go
@@ -40,6 +40,7 @@ func GetSession() (*mgo.Session, error) {
 
 }
 
+// GetDatabase returns database reference
 func GetDatabase(dbSession *mgo.Session) mgo.Database{
 	return	mgo.Database{
 
@@ -56,8 +57,6 @@ func GetCollection(dbSession *mgo.Session,collectionName string) *mgo.Collection
 }
 
 // InsertData inserts an entry in the specified collection name using the provided db session
-
-// It returns true if the record is inserted successfully.
 func InsertData(collectionName string, entry interface{}) error {
 	// Create DB Session
 	dbSession, err := GetSession()
@@ -78,6 +77,29 @@ func InsertData(collectionName string, entry interface{}) error {
 	return nil
 }
 
+// UpdateData updates an entry in the specified collection name using the provided db session
+func UpdateData(collectionName string,id ,entry interface{}) error {
+	// Create DB Session
+	dbSession, err := GetSession()
+	if err != nil {
+		Logger(Panic, err)
+		return err
+	}
+	// Close DB connection after this method is executed.
+	defer dbSession.Close()
+	//Initialize DB Collection here
+	collection := GetCollection(dbSession,collectionName)
+	err = collection.UpdateId(id,entry)
+	if err != nil {
+
+		return err
+
+	}
+	return nil
+}
+
+// IsExistingRecord tests the existance of a record with the provided conditions.
+// It returns true if the record is available in the collection.
 func IsExistingRecord(collectionName string, condition bson.M ) ( found bool,err error){
 	// Create DB Session
 	dbSession, err := GetSession()
@@ -92,4 +114,38 @@ func IsExistingRecord(collectionName string, condition bson.M ) ( found bool,err
 	count,err := collection.Find(condition).Count()
 	found = count>0
 	return  found,err
+}
+
+// GetaRecord returns a record that satisfies the provided conditions.
+// It returns nil if the record is unavailable in the collection.
+func GetaRecord(collectionName string, condition bson.M) ( record interface{},err error){
+	// Create DB Session
+	dbSession, err := GetSession()
+	if err != nil {
+		Logger(Panic, err)
+		return nil,err
+	}
+	// Close DB connection after this method is executed.
+	defer dbSession.Close()
+	//Initialize DB Collection here
+	collection := GetCollection(dbSession,collectionName)
+	err = collection.Find(condition).One(&record)
+	return  record,err
+}
+
+// FindByID returns a record that satisfies the provided conditions.
+// It returns nil if the record is unavailable in the collection.
+func FindByID(collectionName string,id string ) ( record interface{},err error){
+	// Create DB Session
+	dbSession, err := GetSession()
+	if err != nil {
+		Logger(Panic, err)
+		return nil,err
+	}
+	// Close DB connection after this method is executed.
+	defer dbSession.Close()
+	//Initialize DB Collection here
+	collection := GetCollection(dbSession,collectionName)
+	err = collection.FindId(bson.ObjectIdHex(id)).One(&record)
+	return  record,err
 }
